8466ae74f8b07d08226793124936830257882e5c
Lucas Ielli <lucas.ielli.pro@gmail.com>
2022-09-30 11:32:55 +0200

    Create second part of script for animate something

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const animation = document.getElementById("animateLorem");
 animation.addEventListener("click", () => {
   console.log("Salut je fonctionnne");
 });
+
+console.log("Ceci est la deuxième partie");
